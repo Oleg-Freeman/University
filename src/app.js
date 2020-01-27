a06d5f812472b9e46357d817b4c58292d444a7de
Oleg Freeman <av.ssf4@gmail.com>
2020-01-27 17:55:55 +0200

    connect db
    mongodb connection added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const connectDb = require('./db');
 
 require('dotenv').config({ path: './src/config/.env' });
 
 const app = express();
+
 const port = process.env.PORT || 5000;
+const uriDb = process.env.ATLAS_URI;
+
+connectDb(uriDb);
 
 app.use(cors());
 app.use(express.json());
